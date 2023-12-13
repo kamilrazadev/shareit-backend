05f150ed6c2095c8f8a81dd9e3df18db53686f86
Kamil Raza <m.kamilrazabhatti@gmail.com>
2023-12-13 08:25:10 +0500

    connected to frontend

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const cors = require("cors");
+const port = 3100;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json({
